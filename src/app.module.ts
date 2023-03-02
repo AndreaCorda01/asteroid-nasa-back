3eafa28e584b5c952adeb0fe5bbd16af2f454d8e
Andrea Corda <a.corda96@gmail.com>
2023-03-02 06:24:09 +0100

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AsteroidsModule } from './modules/asteroids/asteroids.module';
 
 @Module({
-  imports: [AsteroidsModule],
+  imports: [AsteroidsModule, ConfigModule.forRoot(),],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+const configSv = new ConfigService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(Number(configSv.get('APP_PORT', 3000)),String(configSv.get('APP_HOST', "localhost")));
 }
 bootstrap();
